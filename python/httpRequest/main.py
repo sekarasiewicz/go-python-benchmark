f2de6dfd30703247bd8fe7b53ad1571105f906a2
sebastian <sebastian.karasiewicz@sofomo.com>
2020-01-19 19:37:04 +0100

    Add http request server

---
+++ python/httpRequest/main.py
@@ -0,0 +1,37 @@
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 9000
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self.respond({'status': 200})   
+
+    def handle_http(self, status_code, path):
+        self.send_response(status_code)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        print("Hello world!")
+        return bytes("", 'UTF-8')
+
+    def respond(self, opts):
+        response = self.handle_http(opts['status'], self.path)
+        self.wfile.write(response)
+
+if __name__ == '__main__':
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
